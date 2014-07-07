4628b09af45ce2a2721be1c133109c2694d5ee7f
gr1ph00n <gr1ph00n@gmail.com>
2014-07-07 12:01:42 +0200

    added logger

--- src/simple/FastaMapper.java
+++ src/simple/FastaMapper.java
@@ -1,10 +1,23 @@
 package simple;
 
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class FastaMapper extends Mapper<LongWritable, Text, LongWritable, Text>  
 {
+	private static Log logger = LogFactory.getLog(FastaMapper.class);
 
+	@Override
+	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException 
+	{
+		
+	}
+	
+	
+	
 }
