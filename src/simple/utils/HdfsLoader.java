6398fccbd2ee8de91f111523914e6880dc566aa1
gr1ph00n <gr1ph00n@gmail.com>
2014-07-07 11:46:09 +0200

    added hadoop libs, prepared hdfslaoder

---
+++ src/simple/utils/HdfsLoader.java
@@ -0,0 +1,26 @@
+package simple.utils;
+
+import java.util.Map;
+
+public class HdfsLoader 
+{
+	private static final String DELIMITER = "$$$";
+	private Map<String, Object> checksums;
+
+	public HdfsLoader(String inputDirPath)
+	{
+		
+	}
+	
+	public String processInputDir()
+	{
+		
+		return "";
+	}
+	
+	public Map<String, Object> getChecksums()
+	{
+		return checksums;
+	}
+	
+}
